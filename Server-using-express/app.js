4eaac40754b1072c1506cb23991284b1f261a16c
yadavgopinath <yadavgopinath93@gmail.com>
2024-08-21 00:36:36 +0530

    using middelware for adding products

--- Server-using-express/app.js
+++ Server-using-express/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+
 const app=express();
 
-app.use((req,res,next)=>{
-    console.group('In the middleware');
-    next();
-});
-app.use((req,res,next)=>{
-    console.group('In the next middleware');
-    res.send('<h1>Hello from express</h1>')
-});
+app.use(bodyparser.urlencoded({extended:false}));
 
+app.use('/add-product',(req,res,next)=>{
+        res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
+    });
+app.use('/product',(req,res,next)=>{
+    console.log('Product='+req.body.title);
+    console.log('size='+req.body.size);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
+    console.log("in to middlewaree");
+    res.send('<h1>Hello from express</h1>');
+});
 
 app.listen(3000);
 
